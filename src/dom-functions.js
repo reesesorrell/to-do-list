f916b242e402a4e0125e087709111311e1f4ffa6
Reese Sorrell <joreesesorrell@gmail.com>
2023-02-06 21:29:46 -0600

    Add create project name form function
    Create text input box when add project button is clicked.

--- src/dom-functions.js
+++ src/dom-functions.js
@@ -1,7 +1,19 @@
 import {Todo, Project} from "./object-functions";
+import displayAdder from "./helper";
 
 const makeProjectForm = () => {
-    console.log('working')
+    const newProjectButton = document.getElementById("new-project-button");
+    newProjectButton.remove();
+
+    const sideBar = document.getElementById("side-bar");
+    const projectFormContainer = displayAdder.createDiv(sideBar, '', 'project-form-container');
+    
+    const projectForm = document.createElement('input');
+    projectForm.id = 'project-form';
+    projectForm.type = 'text';
+    projectForm.name = 'project-form';
+    projectForm.required = true;
+    projectFormContainer.appendChild(projectForm);
 }
 
 export {makeProjectForm};
